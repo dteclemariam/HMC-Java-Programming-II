a04c80040a9dd9bd42008ff76c103f80bb730a7e
Daniel Teclemariam <daniel.tecle@hotmail.com>
2021-08-22 23:21:52 +0200

    gtg

--- part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,27 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int count = 0;
 
+
+        while(true){
+            int numb = Integer.valueOf(scanner.nextLine());
+
+            if (numb == 0){
+                break;
+            }
+
+            if (numb > 0){
+                sum += numb;
+                count++;
+            }
+        }
+
+        if (sum == 0){
+            System.out.println("Cannot calculate the average");
+        }else {
+            System.out.println((1.0 * sum) / count);
+        }
     }
 }
--- part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
+++ part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
@@ -5,16 +5,44 @@ public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int first = 0;
+        int second = 0;
+        int max = 100;
+        int minimum = 0;
 
 
         while (true) {
-            System.out.print("> ");
+            //System.out.print("> ");
+            System.out.println("First: " + first + "/100");
+            System.out.println("Second: " + second + "/100");
 
             String input = scan.nextLine();
             if (input.equals("quit")) {
                 break;
             }
 
+            String [] parts = input.split(" ");
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
+
+            if (command.equals("add") && amount > 0) {
+                first = (first + amount) < max? first + amount : max;
+            }
+
+            if (command.equals("move") && amount > 0){
+                if (amount > first){
+                    amount = first;
+                }
+
+                first -= amount;
+                second = (second + amount) < max? second + amount : max;
+            }
+
+            if (command.equals("remove") && amount > 0){
+                second = (second - amount) > 0? second - amount : minimum;
+            }
+
+            System.out.println("");
         }
     }
 
