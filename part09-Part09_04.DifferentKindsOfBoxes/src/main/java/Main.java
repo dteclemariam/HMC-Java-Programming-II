d4d6513e3fe76be0388d5d90b13c2ab472f3f8cb
Daniel Teclemariam <daniel.tecle@hotmail.com>
2021-09-19 18:50:02 +0200

    Added some testcode to main method

--- part09-Part09_04.DifferentKindsOfBoxes/src/main/java/Main.java
+++ part09-Part09_04.DifferentKindsOfBoxes/src/main/java/Main.java
@@ -5,12 +5,26 @@ public class Main {
     public static void main(String[] args) {
         // You can test your program here
 
-        Box box = new BoxWithMaxWeight(5);
-        box.add(new Item("a", 1));
-        box.add(new Item("b", 2));
-        box.add(new Item("c", 2));
-        box.add(new Item("d", 1));
-        System.out.println(box.isInBox(new Item("c")));
-        System.out.println(box.isInBox(new Item("d")));
+        Box boxWithMaxWeight = new BoxWithMaxWeight(5);
+        boxWithMaxWeight.add(new Item("a", 1));
+        boxWithMaxWeight.add(new Item("b", 2));
+        boxWithMaxWeight.add(new Item("c", 2));
+        boxWithMaxWeight.add(new Item("d", 1));
+        System.out.println(boxWithMaxWeight.isInBox(new Item("c")));
+        System.out.println(boxWithMaxWeight.isInBox(new Item("d")));
+
+        OneItemBox oneItemBox = new OneItemBox();
+        oneItemBox.add(new Item("Saludo", 5));
+        oneItemBox.add(new Item("Pirkka", 5));
+
+        System.out.println(oneItemBox.isInBox(new Item("Saludo")));
+        System.out.println(oneItemBox.isInBox(new Item("Pirkka")));
+
+        MisplacingBox misplacingBox = new MisplacingBox();
+        misplacingBox.add(new Item("Saludo", 5));
+        misplacingBox.add(new Item("Pirkka", 5));
+
+        System.out.println(misplacingBox.isInBox(new Item("Saludo")));
+        System.out.println(misplacingBox.isInBox(new Item("Pirkka")));
     }
 }
