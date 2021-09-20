746ecdeecca3e6dba2f46e0d456a32241de3cb74
Daniel <daniel.tecle@hotmail.com>
2021-09-20 23:11:15 +0200

    Comparable interface

--- part10-Part10_11.WageOrder/src/main/java/MainProgram.java
+++ part10-Part10_11.WageOrder/src/main/java/MainProgram.java
@@ -6,16 +6,20 @@ public class MainProgram {
 
     public static void main(String[] args) {
         ArrayList<Human> humans = new ArrayList<>();
-        humans.add(new Human("Matti", 150000));
-        humans.add(new Human("Merja", 500));
+        humans.add(new Human("Matti", 150_000));
+        humans.add(new Human("Merja", 50_000_000));
         humans.add(new Human("Pertti", 80));
 
         System.out.println(humans);
+        //with stream
+        humans.stream()
+                .sorted()
+                .forEach(human -> System.out.println(human));
 
         /*
          * Uncomment the comment below when you have completed the compareTo-method.
          */
-        //Collections.sort(humans);
+        Collections.sort(humans);
         System.out.println(humans);
 
     }
--- part10-Part10_12.StudentsOnAlphabeticalOrder/src/main/java/MainProgram.java
+++ part10-Part10_12.StudentsOnAlphabeticalOrder/src/main/java/MainProgram.java
@@ -1,10 +1,22 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainProgram {
 
     public static void main(String[] args) {
-//        Student first = new Student("jamo");
-//        Student second = new Student("jamo1");
-//        System.out.println(first.compareTo(second));
+        List<Student> students = new ArrayList<>();
+        Student first = new Student("jamo");
+        students.add(first);
+        Student second = new Student("jamo1");
+        students.add(second);
+        Student third = new Student("Aaron");
+        students.add(third);
+        System.out.println(first.compareTo(second));
+        System.out.println();
+
+        students.stream()
+                .sorted()
+                .map(student -> student.getName())
+                .forEach(name -> System.out.println(name));
     }
 }
