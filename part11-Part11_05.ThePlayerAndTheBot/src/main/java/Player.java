544494f49c44a644cef7e06e41c9a663781ba000
Daniel Teclemariam <daniel.tecle@hotmail.com>
2021-12-09 23:59:59 +0100

    DFN

---
+++ part11-Part11_05.ThePlayerAndTheBot/src/main/java/Player.java
@@ -0,0 +1,11 @@
+public class Player {
+    private String name;
+
+    public void play(){
+        //do something
+    }
+
+    public void printName(){
+        System.out.println(name);
+    }
+}
