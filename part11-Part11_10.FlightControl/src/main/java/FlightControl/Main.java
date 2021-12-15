1cf706b4733b1f3065af97816dc99580b5261dd1
Daniel <daniel.tecle@hotmail.com>
2021-12-15 23:23:58 +0100

    Done with JPII - 11 - Packages

--- part11-Part11_10.FlightControl/src/main/java/FlightControl/Main.java
+++ part11-Part11_10.FlightControl/src/main/java/FlightControl/Main.java
@@ -1,9 +1,16 @@
 package FlightControl;
 
 
+import FlightControl.ui.TextUI;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // Write the main program here. It is useful to create some classes of your own.
+        Scanner scanner = new Scanner(System.in);
+        TextUI ui = new TextUI(scanner);
+        ui.start();
     }
 }
